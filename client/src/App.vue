c514607660355c69772ec2fa4c96ccd3aba6f50a
JewelYueng <414608192@qq.com>
2017-06-14 17:16:17 +0800

    init

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+  import Home from './components/home/Index.vue'
+  export default {
+    name: 'app',
+    components: {
+        Home
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .border {
+    border: 1px solid red;
+  }
+
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ client/src/components/home/LeftSide.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="left-side">
+
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  export default{
+      data(){
+          return {
+              msg: 'leftSide'
+          }
+      }
+  }
+</script>
