d6ff64cea705cc7344625c10f387bc13f6cb4bd5
JewelYueng <414608192@qq.com>
2017-06-16 20:30:04 +0800

    删掉target文件夹

--- client/src/components/home/LeftSide.vue
+++ client/src/components/home/LeftSide.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="left-side">
-
+    <div id="logo-box">
+      <img src="" alt="Logo">
+    </div>
+    <div id="username">
+      {{username}}
+    </div>
+    <div class="tabs">
+      <div class="tab">用户列表</div>
+      <div class="tab">日志列表</div>
+      <div class="tab">分享列表</div>
+      <div class="tab">算法列表</div>
+    </div>
+    <div id="logout">
+      <a href="">退出</a>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped rel="stylesheet/less">
 
   @import '~assets/colors.less';
+  @import "~assets/layout.less";
 
   .left-side {
     background-color: @main_green;
-    width: 425px;
+    width: @left_side_width;
     height: 800px;
+  }
+  #username{
+    font-size: 24px;
+    font-weight:bold;
+  }
+  .tab{
+    margin: 0 auto;
+    font-size: 20px;
+    line-height: 45px;
+    color: white;
+    width: 170px;
+    height: 45px;
+    cursor: pointer;
+  }
+  .tab:hover{
+    border: 2px solid white;
+    border-radius: 5px;
+  }
+  .selected-tab{
+    background-color: #3c735a;
   }
 </style>
 
@@ -20,6 +55,7 @@
     data(){
       return {
         msg: 'leftSide',
+        username: '张三'
       }
     },
     created() {
