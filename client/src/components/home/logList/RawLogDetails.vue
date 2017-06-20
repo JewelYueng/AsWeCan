5c6f75168f5b29ef630f5f19b0166f07cb87536f
JewelYueng <414608192@qq.com>
2017-06-20 12:04:19 +0800

    修改了一下static内部的文件路径，增加img文件夹

--- client/src/components/home/logList/RawLogDetails.vue
+++ client/src/components/home/logList/RawLogDetails.vue
@@ -25,7 +25,7 @@
         <li><input type='checkbox' v-model="checkAll">&nbsp;文件名&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;日期&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;规范化日志&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;事件日志</li>
         <hr>
         <template v-for="item in items">
-          <li><input type='checkbox' v-model="checked"  :value="item.id"  @click="currClick(item,$index)">{{item.log_name}}&#12288;&#12288;<img src="static/img/generator_color.png"><img src="static/img/download_color.png"><img src="static/img/share_color.png">{{item.create_date}}&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;{{item.normal_log}}&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;{{item.event_log}}</li>
+          <li><input type='checkbox' v-model="checked"  :value="item.id"  @click="currClick(item,$index)">{{item.log_name}}&#12288;&#12288;<img src="static/img/generator_color.png" title="生成规范化日志"><img src="static/img/download_color.png"><img src="static/img/share_color.png">{{item.create_date}}&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;{{item.normal_log}}&#12288;&#12288;&#12288;&#12288;&#12288;&#12288;{{item.event_log}}</li>
           <hr>
         </template>
       </ul>
