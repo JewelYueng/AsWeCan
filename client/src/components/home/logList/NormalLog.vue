5327019d756a70a294d1a3b112266f6b618b0a4d
NezumiLsy <642977047@qq.com>
2017-06-22 17:46:48 +0800

    规范化日志接口

--- client/src/components/home/logList/NormalLog.vue
+++ client/src/components/home/logList/NormalLog.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="normal-log">
     <div class="head">
-      <a href="" class="button"><img src="static/img/upload.png">上传</a>
-      <a href="" class="button"><img src="static/img/share_white.png">分享</a>
+      <a href="" class="button" @click="upload"><img src="static/img/upload.png">上传</a>
+      <a href="" class="button" @click="shareSome"><img src="static/img/share_white.png">分享</a>
       <input type="text" class="search" placeholder="请输入关键字"><img id="search_button" src="static/img/search.png">
     </div>
     <div class="head-2"><span>全部文件，共{{amount}}个</span><span>关联文件</span></div>
@@ -17,9 +17,24 @@
       </div>
       <div class="list" v-for="(item,index) in items">
         <div><input type="checkbox" v-model="checked" :value="item.id" @click="currClick(item,index)">
-          <span>{{`${item.normalLog.logName}.${item.normalLog.format}`}}</span></div>
-        <div><img class="process_button" title="生成事件日志" v-on:click="tranferToEvent(index)"
+          <span style="cursor:pointer">
+            {{`${item.normalLog.logName}.${item.normalLog.format}`}}</span></div>
+        <div>
+          <img  style="cursor:pointer" class="process_button" title="生成事件日志"
+                  v-on:click="transferToEvent(index)"
                   src="static/img/process_color.png">
+          <img @click="download(index)"  style="cursor:pointer"
+               class="download_button" title="下载" src="static/img/download_color.png">
+          <img @click="share(index)"  style="cursor:pointer"
+               class="share_button" title="分享" src="static/img/share_color.png">
+          <img @click="deleteLog(index)"  style="cursor:pointer"
+               class="delete_button" title="删除" src="static/img/delete_color.png">
+        </div>
+        <div>{{new Date(item.normalLog.createDate).toString()}}</div>
+        <div><a href="">
+          {{item.rawLog ? `${item.rawLog.logName}.${item.rawLog.format}` : '无'}}</a></div>
+        <div><a href="">
+          {{item.eventLog ? `${item.eventLog.logName}.${item.eventLog.format}` : '无'}}</a></div>
           <img class="download_button" title="下载" src="static/img/download_color.png">
           <img class="share_button" title="分享" src="static/img/share_color.png"></div>
         <div>{{new Date(item.normalLog.createDate).toString()}}</div>
@@ -27,7 +42,6 @@
         <div>{{item.eventLog ? `${item.eventLog.logName}.${item.eventLog.format}` : '无'}}</div>
       </div>
     </div>
-  </div>
 </template>
 
 <style lang="less" scoped rel="stylesheet/less">
@@ -72,21 +86,7 @@
     position: relative;
     left: -40px;
     top: 5px;
-  }
-
-  .button {
-    color: white;
-    font-size: 24px;
-    text-decoration: none;
-    height: @log_button_height;
-    width: @log_button_width;
-    border-radius: @log_button_border-radius;
-    background-color: @main_green;
-    img {
-      width: 30px;
-      height: 30px;
-      vertical-align: text-top;
-    }
+    cursor:pointer;
   }
 
   .list:hover {
@@ -197,8 +197,77 @@
           }
         }
       },
-      tranferToEvent: function (index) {
-        console.log(index)
+
+      upload(){
+        this.$modal({type: 'upload', data: {type: 'normal'}}).then((res) => {
+          console.log(res)
+        })
+      },
+
+      transferToEvent: function (index) {
+        this.$api({method: 'toEventLog',query: {id:this.items[index].normalLog.id}}).then((res) =>{
+            console.log(res.data)
+
+        })
+      },
+
+      download: function(index){
+          this.$api({method: "downLoadNormalLog", query: {id: this.items[index].normalLog.id}}).then((res) => {
+              console.log(res.data)
+            this.createAndDownloadFile('normal_log', res.data)
+          })
+      },
+      createAndDownloadFile(fileName, content) {
+        let aTag = document.createElement('a');
+        let blob = new Blob([content]);
+        aTag.download = fileName;
+        aTag.href = URL.createObjectURL(blob);
+        aTag.click();
+        URL.revokeObjectURL(blob);
+      },
+
+      deleteLog: function(index){
+        this.$api({method: 'deleteNormalLog',options: {body: {idList: [this.items[index].normalLog.id]}}}).then((res) =>{
+          console.log(res.data)
+
+        })
+      },
+
+      shareSome(){
+        this.$api({method: 'shareEventLog', body: {idList: this.checked}}).then( res => {
+          if(res.data.code === 1){
+            this.$hint('分享成功', 'success')
+          }else {
+            this.$hint('分享失败', 'warn')
+          }
+
+        })
+      },
+
+      share(index){
+        if(this.items[index].eventLog.isShared === 0) {
+          this.$api({method: 'shareEventLog', body: {idList: [this.items[index].eventLog.id]}}).then(res => {
+            if (res.data.code === 1) {
+              this.$hint('分享成功', 'success')
+            } else {
+              this.$hint('分享失败', 'warn')
+            }
+          })
+        }else{
+          this.$api({method: 'unShareEventLog', body: {idList: [this.items[index].eventLog.id]}}).then(res => {
+            if (res.data.code === 1) {
+              this.$hint('取消分享成功', 'success')
+            } else {
+              this.$hint('取消分享失败', 'warn')
+            }
+          })
+        }
+      },
+
+      search: function(){
+          this.$api({method: 'searchNormalLog',query:{keyWord:keyWord}}).then((res)=>{
+              console.log(res)
+          })
       }
     },
     watch: {
