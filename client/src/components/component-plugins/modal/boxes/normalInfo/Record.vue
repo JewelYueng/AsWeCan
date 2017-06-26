32150131e5f6c018b81dcd7305f86ad5535ef231
NezumiLsy <642977047@qq.com>
2017-06-26 15:56:26 +0800

    规范化弹窗记录格式配置表格完善

--- client/src/components/component-plugins/modal/boxes/normalInfo/Record.vue
+++ client/src/components/component-plugins/modal/boxes/normalInfo/Record.vue
@@ -1,9 +1,41 @@
 <template id="table3" >
   <div>
+    <div style="text-align: left;margin-top: 10px;margin-left: 10px;">
+      <el-button size="small" style="width: 80px;font-size:15px" @click="addBlankRow">添加</el-button>
+    </div>
     <el-table :data="record" border style="top: 90px;width: 90%;margin: auto;">
-      <el-table-column prop="item" label="原数据项分隔符"></el-table-column>
-      <el-table-column prop="name" label="原名称值分隔符"></el-table-column>
-      <el-table-column prop="null" label="原空值分隔符"></el-table-column>
+      <el-table-column prop="item" label="原数据项分隔符">
+        <template scope="scope">
+          <el-input v-model="editing.item" v-show="isEditing(scope.$index)"></el-input>
+          <div v-show="!isEditing(scope.$index)">{{scope.row.item}}</div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="原名称值分隔符">
+        <template scope="scope">
+          <el-input v-model="editing.name" v-show="isEditing(scope.$index)"></el-input>
+          <div v-show="!isEditing(scope.$index)">{{scope.row.name}}</div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="null" label="原空值分隔符">
+        <template scope="scope">
+          <el-input v-model="editing.null" v-show="isEditing(scope.$index)"></el-input>
+          <div v-show="!isEditing(scope.$index)">{{scope.row.null}}</div>
+        </template>
+      </el-table-column>
+      <el-table-column>
+        <template scope="scope" class="btn-column">
+          <el-button
+            size="small"
+            @click="handleEdit(scope.$index, scope.row)"
+            v-show="!isEditing(scope.$index)">编辑
+          </el-button>
+          <el-button
+            size="small"
+            @click="saveEdit(scope.$index, scope.row)"
+            v-show="isEditing(scope.$index)">保存
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -15,6 +47,7 @@
   export default {
     data(){
       return {
+        editingRow: -1,
         record: [
           {
             item: '/t',
@@ -22,6 +55,28 @@
             null: ' '
           }
         ]
+      }
+    },
+    methods: {
+      //      表格的操作函数
+      addBlankRow(){
+        this.record.push({})
+      },
+      handleEdit(index, row){
+        this.editingRow = index
+      },
+      saveEdit(index, row){
+        this.record[index] = this.editing
+        this.editingRow = -1
+        this.$emit('SAVE_RECORD', this.record)
+      },
+      isEditing(index){
+        return index === this.editingRow
+      },
+    },
+    computed: {
+      editing(){
+        return this.editingRow === -1 ? {} : this.record[this.editingRow]
       }
     }
   }
