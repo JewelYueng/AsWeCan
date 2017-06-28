b61f3ce5d3c1ee1fcc270e5081f2b7d50dfdb235
NezumiLsy <642977047@qq.com>
2017-06-28 11:13:09 +0800

    挖掘页面

---
+++ client/src/components/home/processMining/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="diagram-list">
+    <div class="left-side">
+      <el-menu :default-active="current_index" class="el-menu-vertical-demo" @select="handleSelect">
+        <el-menu-item index="1">力导向图</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">流程模型</template>
+          <el-menu-item index="2-1">流程图</el-menu-item>
+          <el-menu-item index="2-2">工作流图</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3">桑基图</el-menu-item>
+      </el-menu>
+    </div>
+    <div id="right-window">
+      <component :is="current_view"></component>
+    </div>
+  </div>
+</template>
+
+<style lang="less" rel="stylesheet/less">
+  @import '~assets/colors.less';
+  @import "~assets/layout.less";
+
+  .left-side {
+    background-color: @light_theme;
+    width: @left_side_width;
+    height: @main_height;
+    box-sizing: border-box;
+  }
+
+  .diagram-list {
+    display: flex;
+    flex-direction: row;
+  }
+
+  #right-window {
+    width: @right_side_width;
+    height: @main_height - 30px;
+    box-sizing: border-box;
+    padding: 30px 30px 0 30px;
+    overflow: auto;
+  }
+  .relation-logs{
+    cursor: pointer;
+  }
+</style>
+
+<script>
+  import Sankey from "./sankey.vue"
+
+  import {mapActions} from 'vuex'
+
+  export default{
+    data(){
+      return {
+        msg: 'leftSide',
+        active_index: '1',
+        view_dict: {
+         // "1":
+          //"2-1":
+          //"2-2":
+          "3": Sankey
+        }
+      }
+    },
+    components: {
+        Sankey
+    },
+    created(){
+      this.changeHomePath('/')
+    },
+    methods: {
+      ...mapActions(['changeHomePath','changeFilePath']),
+      handleSelect(key, keyPath) {
+        this.active_index = key
+        this.changeFilePath(key)
+      }
+    },
+    computed: {
+      current_view() {
+        return this.view_dict[this.$store.getters.file_path || '1']
+      },
+      current_index(){
+        return this.$store.getters.file_path
+      }
+    }
+  }
+</script>
