896238eb876f8cb35cbba9cceadc931e26e500bf
yq-ni <yq.ni@qq.com>
2017-07-08 10:49:22 +0800

    persistentRememberMe

--- server/ProcessMining/src/main/java/org/k2/processmining/controller/admin/AdminUserController.java
+++ server/ProcessMining/src/main/java/org/k2/processmining/controller/admin/AdminUserController.java
@@ -2,8 +2,12 @@ package org.k2.processmining.controller.admin;
 
 import org.k2.processmining.controller.IdListForm;
 import org.k2.processmining.model.UserState;
+import org.k2.processmining.model.user.User;
+import org.k2.processmining.security.user.MyUserDetails;
 import org.k2.processmining.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Aria on 2017/7/3.
@@ -25,11 +28,15 @@ public class AdminUserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    private SessionRegistryImpl sessionRegistry;
+
     @RequestMapping(value = "/active",method = RequestMethod.POST)
     public @ResponseBody
     Object activeUser(@Valid @RequestBody IdListForm idListForm){
         Map map = new HashMap();
         userService.updateStateByUserId(idListForm.getIdList(), UserState.ACTIVE.getValue());
+        expireUser(new HashSet<>(idListForm.getIdList()));
         map.put("code",200);
         return map;
     }
@@ -39,6 +46,7 @@ public class AdminUserController {
     Object freezeUser(@Valid @RequestBody IdListForm idListForm){
         Map map = new HashMap();
         userService.updateStateByUserId(idListForm.getIdList(),UserState.FORBIDDEN.getValue());
+        expireUser(new HashSet<>(idListForm.getIdList()));
         map.put("code",200);
         return map;
     }
@@ -58,5 +66,21 @@ public class AdminUserController {
         Map map = new HashMap();
         map.put("users",userService.getAllUsers());
         return map;
+    }
+
+    private void expireUser(Set<String> ids) {
+        List<Object> principals = sessionRegistry.getAllPrincipals();
+        for (Object principal : principals) {
+            if (principal instanceof MyUserDetails) {
+                User user = ((MyUserDetails) principal).getUser();
+                String id = user.getId();
+                if (ids.contains(id)) {
+                    List<SessionInformation> sessionInformationList = sessionRegistry.getAllSessions(principal, false);
+                    for (SessionInformation sessionInformation :sessionInformationList) {
+                        sessionInformation.expireNow();
+                    }
+                }
+            }
+        }
     }
 }
